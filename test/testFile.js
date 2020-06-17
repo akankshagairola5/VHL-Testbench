00bd3e80a65c398109a4749980980b78d8b759d6
akankshagairola5 <35727466+akankshagairola5@users.noreply.github.com>
2020-06-17 15:38:32 +0530

    update test

--- test/testFile.js
+++ test/testFile.js
@@ -1,4 +1,15 @@
 const { expect } = require('chai');
+const puppeteer = require('puppeteer');
+
+const _ = require('lodash');
+
+const opts = {
+    headless: false,
+    slowMo: 0,
+    timeout: 0,
+    defaultViewport: null,
+    args: ['--start-maximized', '--window-size=1920,1080']
+}
 
 describe('Test for VHL testbench', async () => {
     let page;
@@ -6,16 +17,24 @@ describe('Test for VHL testbench', async () => {
     before(async () => {
 
         /* before hook for mocha testing */
+        browser = await puppeteer.launch(opts);
 
         page = (await browser.pages())[0]
-      //  page = await browser.newPage()
+        //  page = await browser.newPage()
         await page.goto("https://lti-gateway-testing.s3.amazonaws.com/lti-client.html");
+
+        console.log("launched...")
+
         // await page.setViewport( { width: 1366, height: 657} );
     });
 
     after(async function() { /* after hook for mocah testing */
 
+
         await page.close();
+
+        browser.close();
+       
     });
 
     it('Launch VHL Testbench', async () => { /* simple test case */
@@ -23,6 +42,7 @@ describe('Test for VHL testbench', async () => {
         const input = await page.$('input[id="call_url"]');
         await input.click({ clickCount: 3 })
         await input.type("https://reader.vhlcentral.com/demo/student-edition/comproqa_seneros_1b");
+
 
         const key = await page.$('input[id="secret"]');
         await key.click({ clickCount: 3 })
